c1c742191c098d91a690314057ff2c5f8cd54c16
gakawarstone <pinnekerv@gmail.com>
2022-02-05 20:18:55 +0600

    feat: add scroll to top when switch page

--- client/app.js
+++ client/app.js
@@ -1,19 +1,28 @@
-document.addEventListener('keydown', (event) => {
+document.addEventListener('keydown', event => {
   img = document.querySelector('img')
   img_src = img.getAttribute('src')
   img_number = img_src.slice(4, 7)
 
   if (event.code == 'ArrowRight') {
     img_number = Number(img_number) + 1
+    window.scrollTo(0, 0)
   }
 
   if (event.code == 'ArrowLeft') {
     img_number = Number(img_number) - 1
+    window.scrollTo(0, 0)
   }
 
-  img.setAttribute('src',
+  if (img_number > 18) {
+    chapter_number = document.createElement('h3')
+    chapter_number.innerHTML = 'Chapter 54'
+    document.body.appendChild(chapter_number)
+  }
+
+  img.setAttribute(
+    'src',
     `src/${
-      '0'.repeat(3-String(img_number).length) + String(img_number)
+      '0'.repeat(3 - String(img_number).length) + String(img_number)
     }.png`
   )
 })
